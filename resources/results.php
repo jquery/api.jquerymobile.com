f061311326e58514ba0b19ac53c3ee98aea47069
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-12-16 20:37:19 +0200

    entries and resources: Removing trailing whitespace.

--- resources/results.php
+++ resources/results.php
@@ -1,33 +1,29 @@
-<!DOCTYPE html> 
-<html> 
-<head> 
-	<title>Page Title</title> 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page Title</title>
 
-	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<link rel="stylesheet" href="//code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css" />
 	<script src="//code.jquery.com/jquery-1.9.1.min.js"></script>
 	<script src="//code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
-</head> 
+</head>
 
-<body> 
+<body>
 <div data-role="page" id="formResults" style="max-height:90px; min-height:90px;">
 	<div data-role="header">
 		<h1>Confirm</h1>
 	</div>
     <div data-role="content">
 		<h1>This is the Results page!</h1>
-        
+
         <h2>You Chose:</h2>
 		<?php
 		echo "<p> " . $_REQUEST['choice'] . "</p>";
 		?>
 
-
 	</div>
 </div>
 </body>
 </html>
-	
-
-
